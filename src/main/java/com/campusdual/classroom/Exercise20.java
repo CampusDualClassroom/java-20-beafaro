a05c0f06b995cb42ceba0814415f7c4e7b5aeac6
Beatriz Faro Carrera <farocarrera.beatriz@gmail.com>
2024-10-01 17:53:06 +0200

    Exercicio 20 resolto

--- src/main/java/com/campusdual/classroom/Exercise20.java
+++ src/main/java/com/campusdual/classroom/Exercise20.java
@@ -6,11 +6,20 @@ import java.util.List;
 public class Exercise20 {
 
     public static List<Person> getPeopleList() {
+        List<Person> personList = new ArrayList<>();
 
+        personList.add(new Person("John", "Smith"));
+        personList.add(new Doctor("María", "Montessori", "Educación"));
+        personList.add(new PoliceOfficer("Jake", "Peralta", "B-99"));
+        personList.add(new Teacher("Gregory", "House", "Nefrología e infectología"));
+
+        return personList;
     }
 
     public static void showPeopleDetails(List<Person> stringList) {
-
+        for (Person person : stringList) {
+            person.getDetails();
+        }
     }
 
     public static void main(String[] args) {
